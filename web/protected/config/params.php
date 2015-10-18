c4f48b04c99c8ba4242fb1a836a02b7299d664ab
shangjie <741914101@qq.com>
2015-10-18 21:18:29 +0800

    add boostrap

--- web/protected/config/params.php
+++ web/protected/config/params.php
@@ -2,8 +2,10 @@
 return array(
     //网站所用图片路径
     'image_url' => '/images',
-    //网站所用静态文件路径
+    //网站所用js文件路径
     'script_url' => '/js',
+    //网站所用css文件路径
+    'css_url' => '/js',
     //上传图片路径
     'upload_img_url' => '/upload/images',
     //上传文件路径
