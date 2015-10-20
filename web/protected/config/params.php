5c7e4f832dc11600a8bd83eb3a6f5c26e6d925c3
shangjie <741914101@qq.com>
2015-10-20 20:58:28 +0800

    layout

--- web/protected/config/params.php
+++ web/protected/config/params.php
@@ -3,9 +3,9 @@ return array(
     //网站所用图片路径
     'image_url' => '/images',
     //网站所用js文件路径
-    'js_url' => '/poodle/js',
+    'js_url' => '/js',
     //网站所用css文件路径
-    'css_url' => '/poodle/css',
+    'css_url' => '/css',
     //上传图片路径
     'upload_img_url' => '/upload/images',
     //上传文件路径
--- web/protected/views/layouts/main.php
+++ web/protected/views/layouts/main.php
@@ -8,121 +8,178 @@
 	<script type="text/javascript" src="<?php echo Yii::app()->params->js_url;?>/jquery-1.11.3.min.js"></script>
 	<script type="text/javascript" src="<?php echo Yii::app()->params->js_url;?>/bootstrap/bootstrap-paginator.min.js"></script>
 </head>
-
 <body>
     <div class="container">
     	<div class="row">
     		<div class="span12">
-    			<ul class="nav nav-tabs">
-    				<li class="active">
-    					<a href="#">首页</a>
-    				</li>
-    				<li>
-    					<a href="#">资料</a>
-    				</li>
-    				<li class="disabled">
-    					<a href="#">信息</a>
-    				</li>
-    			</ul>
-    			<!--content开始-->
     			<div class="page-header">
     				<h1>
     					页标题范例 <small>此处编写页标题</small>
     				</h1>
     			</div>
-    			<form class="form-search">
-    				<input class="input-medium search-query" type="text" /> <button type="submit" class="btn">查找</button>
-    			</form>
-    			<ul class="thumbnails">
-    				<li class="span4">
-    					<div class="thumbnail">
-    						<img alt="300x200" src="img/people.jpg" />
-    						<div class="caption">
-    							<h3>
-    								冯诺尔曼结构
-    							</h3>
-    							<p>
-    								也称普林斯顿结构，是一种将程序指令存储器和数据存储器合并在一起的存储器结构。程序指令存储地址和数据存储地址指向同一个存储器的不同物理位置。
-    							</p>
-    							<p>
-    								<a class="btn btn-primary" href="#">浏览</a> <a class="btn" href="#">分享</a>
-    							</p>
-    						</div>
+    			<!-- 菜单开始 -->
+    			<div class="row">
+    				<div class="span2">
+    					<ul class="nav nav-list well">
+    						<li class="nav-header">
+    							列表标题
+    						</li>
+    						<li class="active">
+    							<a href="#">首页</a>
+    						</li>
+    						<li>
+    							<a href="#">库</a>
+    						</li>
+    						<li>
+    							<a href="#">应用</a>
+    						</li>
+    						<li class="nav-header">
+    							功能列表
+    						</li>
+    						<li>
+    							<a href="#">资料</a>
+    						</li>
+    						<li>
+    							<a href="#">设置</a>
+    						</li>
+    						<li class="divider">
+    						</li>
+    						<li>
+    							<a href="#">帮助</a>
+    						</li>
+    					</ul>
+    				</div>
+    			    <!-- 菜单结束-->
+    			    <!-- 内容开始 -->
+    				<div class="span10">
+    					<form class="form-search form-inline">
+    						<input class="input-medium search-query" type="text" /> <button type="submit" class="btn">查找</button>
+    					</form>
+    					<table class="table table-condensed table-bordered table-hover">
+    						<thead>
+    							<tr>
+    								<th>
+    									编号
+    								</th>
+    								<th>
+    									产品
+    								</th>
+    								<th>
+    									交付时间
+    								</th>
+    								<th>
+    									状态
+    								</th>
+    							</tr>
+    						</thead>
+    						<tbody>
+    							<tr>
+    								<td>
+    									1
+    								</td>
+    								<td>
+    									TB - Monthly
+    								</td>
+    								<td>
+    									01/04/2012
+    								</td>
+    								<td>
+    									Default
+    								</td>
+    							</tr>
+    							<tr class="success">
+    								<td>
+    									1
+    								</td>
+    								<td>
+    									TB - Monthly
+    								</td>
+    								<td>
+    									01/04/2012
+    								</td>
+    								<td>
+    									Approved
+    								</td>
+    							</tr>
+    							<tr class="error">
+    								<td>
+    									2
+    								</td>
+    								<td>
+    									TB - Monthly
+    								</td>
+    								<td>
+    									02/04/2012
+    								</td>
+    								<td>
+    									Declined
+    								</td>
+    							</tr>
+    							<tr class="warning">
+    								<td>
+    									3
+    								</td>
+    								<td>
+    									TB - Monthly
+    								</td>
+    								<td>
+    									03/04/2012
+    								</td>
+    								<td>
+    									Pending
+    								</td>
+    							</tr>
+    							<tr class="info">
+    								<td>
+    									4
+    								</td>
+    								<td>
+    									TB - Monthly
+    								</td>
+    								<td>
+    									04/04/2012
+    								</td>
+    								<td>
+    									Call in to confirm
+    								</td>
+    							</tr>
+    						</tbody>
+    					</table>
+    					<div class="pagination pagination-large">
+    						<ul>
+    							<li>
+    								<a href="#">上一页</a>
+    							</li>
+    							<li>
+    								<a href="#">1</a>
+    							</li>
+    							<li>
+    								<a href="#">2</a>
+    							</li>
+    							<li>
+    								<a href="#">3</a>
+    							</li>
+    							<li>
+    								<a href="#">4</a>
+    							</li>
+    							<li>
+    								<a href="#">5</a>
+    							</li>
+    							<li>
+    								<a href="#">下一页</a>
+    							</li>
+    						</ul>
     					</div>
-    				</li>
-    				<li class="span4">
-    					<div class="thumbnail">
-    						<img alt="300x200" src="img/city.jpg" />
-    						<div class="caption">
-    							<h3>
-    								哈佛结构
-    							</h3>
-    							<p>
-    								哈佛结构是一种将程序指令存储和数据存储分开的存储器结构，它的主要特点是将程序和数据存储在不同的存储空间中，进行独立编址。
-    							</p>
-    							<p>
-    								<a class="btn btn-primary" href="#">浏览</a> <a class="btn" href="#">分享</a>
-    							</p>
-    						</div>
-    					</div>
-    				</li>
-    				<li class="span4">
-    					<div class="thumbnail">
-    						<img alt="300x200" src="img/sports.jpg" />
-    						<div class="caption">
-    							<h3>
-    								改进型哈佛结构
-    							</h3>
-    							<p>
-    								改进型的哈佛结构具有一条独立的地址总线和一条独立的数据总线，两条总线由程序存储器和数据存储器分时复用，使结构更紧凑。
-    							</p>
-    							<p>
-    								<a class="btn btn-primary" href="#">浏览</a> <a class="btn" href="#">分享</a>
-    							</p>
-    						</div>
-    					</div>
-    				</li>
-    			</ul>
-    			<div class="pagination">
-    				<ul>
-    					<li>
-    						<a href="#">上一页</a>
-    					</li>
-    					<li>
-    						<a href="#">1</a>
-    					</li>
-    					<li>
-    						<a href="#">2</a>
-    					</li>
-    					<li>
-    						<a href="#">3</a>
-    					</li>
-    					<li>
-    						<a href="#">4</a>
-    					</li>
-    					<li>
-    						<a href="#">5</a>
-    					</li>
-    					<li>
-    						<a href="#">6</a>
-    					</li>
-    					<li>
-    						<a href="#">7</a>
-    					</li>
-    					<li>
-    						<a href="#">8</a>
-    					</li>
-    					<li>
-    						<a href="#">下一页</a>
-    					</li>
-    				</ul>
+    				</div>
+    			    <!-- 内容结束 -->
     			</div>
-    			<script type="text/javascript">
-    		    $(".pagination").bootstrapPaginator({currentPage: 4,totalPages: 10,numberOfPages:5});
-    			</script>
-    			<!--content结束-->
     		</div>
     	</div>
     </div>
+    <script type="text/javascript">
+        $(function(){
+		    $(".pagination").bootstrapPaginator({currentPage: 4,totalPages: 10,numberOfPages:10});
+        });
+    </script>
 </body>
 </html>
